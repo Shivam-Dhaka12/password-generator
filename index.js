908e1c162fcf8b2d751a22985466a36e99ec4a2f
Shivam Dhaka <shivamdhaka1200@gmai.com>
2022-05-04 18:42:13 +0530

    v1.4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function pass() {
     let characters = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
     let password = ""
-    for (let index = 0; index < 10; index++) {
+    for (let index = 0; index < 9; index++) {
         password += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     return password;
